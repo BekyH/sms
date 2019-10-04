4406cf188218fe41e432298e081727b16c142cce
unknown <bereketheramo25@gmail.com>
2019-10-04 15:56:23 +0300

    header completed

---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+<nav>
+    <v-toolbar color="primary" class="white--text"  >
+        <v-toolbar-title>{{title}}</v-toolbar-title>
+        <v-flex md6 offset-2>
+            <v-toolbar-items>
+                <v-btn text class="white--text">Home</v-btn>
+                <v-btn text class="white--text">Aboutus</v-btn>
+                <v-btn text class="white--text">Contactus</v-btn>
+                <v-btn text class="white--text">register</v-btn>
+            </v-toolbar-items>
+
+        </v-flex>
+     
+    </v-toolbar>
+</nav>
+</template>
+<script>
+export default {
+    data: () => ({
+        title:'this is vuejs'
+  }),
+}
+</script>
